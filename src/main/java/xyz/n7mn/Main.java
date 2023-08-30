6449898a91a8d0ac712bdac0bd447ffde580388a
ちのなみ <tino_nanami@7mi.site>
2023-08-30 23:07:13 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/Main.java
@@ -0,0 +1,20 @@
+package xyz.n7mn;
+
+public class Main {
+    public static void main(String[] args) {
+        // HTTP通信を受け取る
+        
+        // HTTP通信からURLを受け取る
+
+        // とりあえず.jpg、.jpeg、.png、.gif、.webp以外は画像じゃないものとみなす
+        
+        // ffmpegでtsにする
+
+        // HLSのm3u8を生成する
+
+        // 見れる用のURLを返す
+
+
+
+    }
+}
